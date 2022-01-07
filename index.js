fea94c6de99dd38193d4a429beaf5b9f1d1c820c
misterkillerman <74764482+misterkillerman@users.noreply.github.com>
2022-01-07 10:36:45 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -9,8 +9,13 @@ function scrollFunction(){
   for (var i = 0; i < bodyChildren.length; i++) {
     var divChildren = bodyChildren[i].children
     for (var k = 0; k < divChildren.length; k++) {
-      var position = divChildren[k].offsetTop - 400
-      if (scrollTop > position ) {
+      var position = divChildren[k].offsetTop - 500
+      if(i == bodyChildren.length - 1 && k == divChildren.length - 1){
+        if(divChildren[k-2].style.opacity == "1"){
+          divChildren[k].style.animationName = "slidein"
+          divChildren[k].style.animationDuration = "2s"
+        }
+      } else if (scrollTop > position ) {
         if (divChildren[k].style.animation == null || divChildren[k].style.animation == "") {
           if (divChildren[k].tagName == "IMG") {
             divChildren[k].style.animationName = "slideinIMG2"
@@ -22,14 +27,14 @@ function scrollFunction(){
               divChildren[k].children[m].style.animationName = "slideinIMG"
               divChildren[k].children[m].style.animationDuration = "2s"
             }
+          } else if (divChildren[k].tagName == "A") {
+            divChildren[k].style.animationName = ""
           } else {
             divChildren[k].style.animationName = "slidein"
             divChildren[k].style.animationDuration = "2s"
           }
-
         }
       }
     }
   }
-  // console.log(scrollTop);
 }
